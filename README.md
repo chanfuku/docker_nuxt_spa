116c06ca639364ef1be9fb6e8a8d0d9f39b24ef2
chanfuku <koheiebato@gmail.com>
2021-09-12 12:35:40 +0900

    docker系, nuxt系, READMEなど全ファイル

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # docker_nuxt_spa
+
+```
+$git clone git@github.com:chanfuku/docker_nuxt_spa.git
+$cd docker_nuxt_spa/docker/nuxt
+$docker-compose up
+※yarn installを実行するため初回は5分~10分程かかります。
+```
+
+- ブラウザで以下が表示されればOK
+```
+http://localhost:3000/
+```
+
+- このプロジェクトは以下の手順で作成しました。
+```
+$cd docker_nuxt_spa/docker/nuxt
+$docker-compose build
+$docker-compose run --rm nuxt npx create-nuxt-app --overwrite-dir .
+このプロジェクトでは下記を選択した。
+✨  Generating Nuxt.js project in .
+? Project name: nuxt_app
+? Programming language: TypeScript
+? Package manager: Yarn
+? UI framework: None
+? Nuxt.js modules: Axios - Promise based HTTP client
+? Linting tools: (Press <space> to select, <a> to toggle all, <i> to invert selection)
+? Testing framework: None
+? Rendering mode: Single Page App
+? Deployment target: Static (Static/Jamstack hosting)
+? Development tools: (Press <space> to select, <a> to toggle all, <i> to invert selection
+)
+? What is your GitHub username? chanfuku
+? Version control system: Git
+```
---
+++ pages/test.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h1>Nuxt.js Rendering mode: SPA(Single Page App)</h1>
+    <p><a href="/">aタグでトップに戻る</a></p>
+    <p><button @click="toTop">window.location.href</button></p>
+    <p><NuxtLink to="/">NuxtLinkでトップに戻る</NuxtLink></p>
+    <p><button @click="push">$router.pushでトップに戻る</button></p>
+
+    <button @click="addDogImage">犬画像追加</button>
+    <div v-if="$fetchState.pending">
+        <img class="loading__img" src="@/assets/img/loading-spin.svg">
+    </div>
+    <div v-else class="imagebox">
+      <img :src="image" v-for="image in images" :key="image">
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  computed: {
+    images (): string[] {
+      return this.$store.state.images
+    },
+  },
+  async fetch(): Promise<void> {
+    // console.log(this.$nuxt.context)
+    console.log('test.vueです')
+    await this.$store.dispatch('fetchRandomDog')
+  },
+  methods: {
+    async addDogImage(): Promise<void> {
+      await this.$fetch()
+    },
+    async toTop(): Promise<void> {
+      window.location.href = '/'
+    },
+    async push(): Promise<void> {
+      this.$router.push('/')
+    },
+  }
+})
+</script>
+
+<style scoped>
+.loading__img {
+  width:30%;
+}
+.imagebox {
+  max-width: 100px;
+}
+</style>
